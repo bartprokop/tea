225dfe37413a2cf4b2f7edca2160b3b401c57940
Bart Prokop <prokop.bart@gmail.com>
2017-03-22 23:43:59 +0000

    more go style encrypt

--- core.go
+++ core.go
@@ -1,20 +1,14 @@
-package stringutil
+package tea
 
-func Encrypt(v uint64, k [4]uint32) uint64 {
-	var v0 uint32 = uint32(v >> 32)
-	var v1 uint32 = uint32(v & 0xffffffff)
+func Encrypt(v0, v1, k0, k1, k2, k3 uint32) (uint32, uint32) {
 	var sum uint32 = 0x0
 	const delta uint32 = 0x9e3779b9
-	var k0 uint32 = k[0]
-	var k1 uint32 = k[1]
-	var k2 uint32 = k[2]
-	var k3 uint32 = k[3]
 	for i := 0; i < 32; i++ {
 		sum += delta
 		v0 += ((v1 << 4) + k0) ^ (v1 + sum) ^ ((v1 >> 5) + k1)
 		v1 += ((v0 << 4) + k2) ^ (v0 + sum) ^ ((v0 >> 5) + k3)
 	}
-	return (uint64(v0) << 32) | uint64(v1)
+	return v0, v1
 }
 
 // void decrypt (uint32_t* v, uint32_t* k) {
--- core_test.go
+++ core_test.go
@@ -1,22 +1,41 @@
-package stringutil
+package tea
 
 import "testing"
 
 func TestEncrypt1(t *testing.T) {
 	cases := []struct {
-		v uint64
-		k [4]uint32
-		e uint64
+		v0, v1         uint32
+		k0, k1, k2, k3 uint32
+		e0, e1         uint32
 	}{
-		{0x0000000000000000, [4]uint32{0, 0, 0, 0}, 0x41ea3a0a94baa940},
-		{0x0102030405060708, [4]uint32{0, 0, 0, 0}, 0x6a2f9cf3fccf3c55},
-		{0x0000000000000000, [4]uint32{0x01234567, 0x12345678, 0x23456789, 0x3456789a}, 0x34e943b0900f5dcb},
-		{0x0102030405060708, [4]uint32{0x01234567, 0x12345678, 0x23456789, 0x3456789a}, 0x773dc179878a81c0},
+		{0x00000000, 0x00000000, 0, 0, 0, 0, 0x41ea3a0a, 0x94baa940},
+		{0x01020304, 0x05060708, 0, 0, 0, 0, 0x6a2f9cf3, 0xfccf3c55},
+		{0x00000000, 0x00000000, 0x01234567, 0x12345678, 0x23456789, 0x3456789a, 0x34e943b0, 0x900f5dcb},
+		{0x01020304, 0x05060708, 0x01234567, 0x12345678, 0x23456789, 0x3456789a, 0x773dc179, 0x878a81c0},
 	}
 	for _, c := range cases {
-		got := Encrypt(c.v, c.k)
-		if got != c.e {
-			t.Errorf("Ecrypt(%q, %q) == %q, want %q", c.v, c.k, got, c.e)
+		e0, e1 := Encrypt(c.v0, c.v1, c.k0, c.k1, c.k2, c.k3)
+		if e0 != c.e0 || e1 != c.e1 {
+			t.Errorf("Broken test vector")
 		}
 	}
 }
+
+// func TestEncrypt2(t *testing.T) {
+// 	cases := []struct {
+// 		v uint64
+// 		k      [4]uint32
+// 		e      uint64
+// 	}{
+// 		{0x0000000000000000, [4]uint32{0, 0, 0, 0}, 0x41ea3a0a94baa940},
+// 		{0x0102030405060708, [4]uint32{0, 0, 0, 0}, 0x6a2f9cf3fccf3c55},
+// 		{0x0000000000000000, [4]uint32{0x01234567, 0x12345678, 0x23456789, 0x3456789a}, 0x34e943b0900f5dcb},
+// 		{0x0102030405060708, [4]uint32{0x01234567, 0x12345678, 0x23456789, 0x3456789a}, 0x773dc179878a81c0},
+// 	}
+// 	for _, c := range cases {
+// 		got := Encrypt(c.v, c.k)
+// 		if got != c.e {
+// 			t.Errorf("Ecrypt(%q, %q) == %q, want %q", c.v, c.k, got, c.e)
+// 		}
+// 	}
+// }
